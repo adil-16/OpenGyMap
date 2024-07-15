e8448e4bf2a65fec2bc34788146bdb86d4652b97
adil-16 <adilmasood833@gmail.com>
2024-07-15 13:38:20 +0500

    Seacrh Bar Added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CiSearch } from "react-icons/ci";
+import SearchBar from "../../components/SeacrhBar/SearchBar";
 
 const Home = () => {
   return (
@@ -70,12 +71,7 @@ const Home = () => {
         </div>
       </div>
       <div className="relative mb-64 mt-16 w-1/2">
-        <CiSearch className="absolute left-6  top-1/2 transform -translate-y-1/2 text-custom-black font-bold text-4xl" />
-        <input
-          placeholder="Search by game, venue, location"
-          type="text"
-          className="w-full p-6 pl-24 border border-gray-300 rounded-full shadow-xl bg-gray-50 text-custom-black dark:bg-white"
-        />
+        <SearchBar />
       </div>
     </div>
   );
