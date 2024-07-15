7832fcf5cab4bbd701b19e7b8b8f5716a7c2cba8
adil-16 <adilmasood833@gmail.com>
2024-07-15 17:32:46 +0500

    Navbar Updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,10 +6,12 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import TimePicker from "react-time-picker";
 import "react-time-picker/dist/TimePicker.css";
+import CustomDateInput from "./Components/CustomDateInput";
 
 const Home = () => {
-  const [selectedDate, setSelectedDate] = useState("");
-  const [selectedTime, setSelectedTime] = useState("");
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedTime, setSelectedTime] = useState();
+
   return (
     <div className="flex flex-col items-center mt-16 px-4">
       <div className="flex flex-col lg:flex-row items-center lg:items-start w-full max-w-screen-2xl">
@@ -96,18 +98,41 @@ const Home = () => {
               className="w-12 h-10"
             />
           </div>
-          <div>
+
+          <div className="relative max-w-sm pl-6">
             <DatePicker
               selected={selectedDate}
               onChange={(date) => setSelectedDate(date)}
-              className="border p-2 rounded-md bg-navbar-gray pl-4"
+              customInput={<CustomDateInput />}
+              className="border rounded-md bg-navbar-gray"
             />
           </div>
-          <div>
-            <TimePicker
-              onChange={setSelectedTime}
-              value={selectedTime}
-              className="border p-2 rounded-md"
+
+          <div class="relative">
+            <div class="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
+              <svg
+                class="w-5 h-5 text-custom-blue dark:text-custom-blue"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </div>
+            <input
+              type="time"
+              id="time"
+              class="bg-gray-50 border leading-none text-custom-black text-sm rounded-lg block w-full p-2.5 dark:bg-gray-100 dark:placeholder-custom-black "
+              min="09:00"
+              max="18:00"
+              // value="00:00"
+              placeholder="Time"
+              required
             />
           </div>
         </div>
