920a1286f729973662d6e207d2b08981bbe97d91
adil-16 <adilmasood833@gmail.com>
2024-07-26 17:56:15 +0500

    [GET] Update get bookings

--- src/firebase/Functions/BookingFunctions.jsx
+++ src/firebase/Functions/BookingFunctions.jsx
@@ -1,5 +1,13 @@
-// BookingFunctions.jsx
-import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
+import {
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "../firebase.config";
 
 export const addBooking = async (bookingData) => {
@@ -34,5 +42,21 @@ export const addBooking = async (bookingData) => {
     }
   } catch (error) {
     console.error("Error adding booking or updating facility: ", error);
+  }
+};
+
+export const getUserBookings = async (uid) => {
+  try {
+    const bookingsRef = collection(db, "bookings");
+    const q = query(bookingsRef, where("createdBy", "==", uid));
+    const querySnapshot = await getDocs(q);
+    const userBookings = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    return userBookings;
+  } catch (error) {
+    console.error("Error fetching user bookings: ", error);
+    throw new Error("Failed to fetch user bookings");
   }
 };
